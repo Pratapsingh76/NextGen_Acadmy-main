031a8df8a736aa6f15f37aee0bb3d323d6726071
Pratapsingh76 <lokeshsuiwal282@gmail.com>
2024-07-10 22:56:08 +0530

    add new css

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.text-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
